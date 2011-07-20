575849782055377ff16440b774aac4698f35af0b
Nick Lott <nick.lott@gmail.com>
2011-07-20 18:42:40 +0100

    power up/down changes so I can play with the extruder more.

--- pinio.c
+++ pinio.c
@@ -10,6 +10,7 @@ void power_off() {
 	WRITE(STEPPER_ENABLE_PIN, STEPPER_ENABLE_INVERT ^ 1);
 	#endif
 	#ifdef	PS_ON_PIN
-		SET_INPUT(PS_ON_PIN);
+	//	Removed this as it turns off the extruder so I can't monitor the temperature
+	//	SET_INPUT(PS_ON_PIN);
 	#endif
 }
